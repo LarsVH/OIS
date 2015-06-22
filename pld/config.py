832a48b345a1c2847b9c7d8bf03561292345feba
Nils Van Geele <nils.vgeele@gmail.com>
2015-06-22 20:04:22 +0200

    Update config.py

--- pld/config.py
+++ pld/config.py
@@ -3,4 +3,4 @@ class Config(object):
     # schema_name = 'pld.pld'
     database_uri = 'mysql://%s:%s@%s/%s' % ('root', '', '127.0.0.1', 'ois')
     # database_uri = 'sqlite:////tmp/test.db'
-    # database_uri = 'virtuoso://dba:ditishetwachtwoordkut@VOS'
+    # database_uri = 'virtuoso://dba:dba@VOS'
